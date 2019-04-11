02c88c660f15e536bd73ffe85d5866da07dedb5e
Quentin <duflotquentin@hotmail.fr>
2019-04-11 16:58:46 +0200

    Adding Const + check-method + constr

--- ub14/Mitarbeiter.java
+++ ub14/Mitarbeiter.java
@@ -5,7 +5,39 @@
  * @author Quentin
  * @version 10/04//2019
  */
-public class Mitarbeiter
+public class Mitarbeiter extends Person
 {
+    //Meldungskonstanten
 
+    private static final String MISSING_AT_FEHLER = "Ihre E-Mail braucht ein @ !";
+    private static final String MISSING_DOT_FEHLER = "Ihre E-Mail braucht ein '.' !";
+    private static final String LEER_EMAIL_FEHLER = "Ihre E-Mail ist leer !";
+
+    //Attribute
+
+    private String eMail;
+
+    //Konstruktor
+
+    public Mitarbeiter(String name, String vorname, String eMail) {
+        super(name, vorname);
+        check(eMail != "", LEER_EMAIL_FEHLER);
+        check(eMail.indexOf('@') != -1, MISSING_AT_FEHLER);
+        check(eMail.indexOf('.') != -1, MISSING_DOT_FEHLER);
+        this.eMail = eMail;
+    }
+
+
+    //Weitere Methode
+
+    /**
+     * Einfache Check-Methode
+     *
+     * @param bedingung Bedingung, die man überprüfen möchtet
+     * @param msg Fehler-Message
+     */
+    private static void check(boolean bedingung, String msg) {
+        if (!bedingung)
+            throw new IllegalArgumentException(msg);
+    }
 }
