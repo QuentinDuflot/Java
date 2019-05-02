814a725e7532e167099573ac7e17df4f4a992085
Duflot <duflotquentin@hotmail.fr>
2019-05-02 21:16:58 +0200

    Beginning Ex2

---
+++ ueb16/Part2/src/NumberCruncherAnonyme.java
@@ -0,0 +1,15 @@
+public class NumberCruncherAnonyme {
+
+    private int groesse;
+    private float[] array;
+
+    public NumberCruncherAnonyme(int groesse)
+    {
+        this.groesse = groesse;
+        this.array = new float[groesse];
+        for(int i = 0; i < groesse; i++)
+        {
+            this.array[i] =  (float)Math.random();
+        }
+    }
+}
