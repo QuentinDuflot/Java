35c21cb162d8f8e4f80f5ecec2d8e93a31dbb8d6
Duflot <duflotquentin@hotmail.fr>
2019-05-10 17:36:21 +0200

    Factorial & Fibo as Lambda + main

--- ueb17/src/Functions.java
+++ ueb17/src/Functions.java
@@ -10,7 +10,7 @@ public class Functions {
     }
 	
 	public void run() {
-		//1.A)
+		//i.)
 		//Anonymous
 		MyFunction xCubeAnonymous = new MyFunction() {
 			public int apply(int x)
@@ -21,7 +21,7 @@ public class Functions {
 		System.out.println("\n x^2 (Anonyme Klasse)");
 		applyAndPrint(xCubeAnonymous, 1, 5);
 		
-		//1.B)
+		//ii.)
 		//Anonymous
 		MyFunction xFactorialAnonymous = new MyFunction() {
 			public int apply(int x)
@@ -37,6 +37,44 @@ public class Functions {
 		System.out.println("\n x! (Anonyme Klasse)");
 		applyAndPrint(xFactorialAnonymous,1,5);
 		
+		//iii.)
+		//Lambda
+		MyFunction xPowerxPlus1 = x -> {
+			
+			int result = x;
+			for(int i = 2; i <= x+1; i++)
+			{
+				result *=x;
+			}
+			return result;
 		
+		};
+		System.out.println("\n x^(x+1) (Lamda-Ausdruck");
+		applyAndPrint(xPowerxPlus1, 1, 5);
+		
+		//iv.)
+		//Lambda
+		MyFunction fibonnacciLambda = x -> {
+			if(x <= 1) {
+				return x;
+			}
+			int fib = 1;
+			int prevFib = 1;
+			
+			for(int i=2; i<x; i++) {
+				int temp = fib;
+				fib+= prevFib;
+				prevFib = temp;
+			}
+			return fib;
+		};
+		System.out.println("\n fibonnacci(x) (Lambda)");
+		applyAndPrint(fibonnacciLambda, 1, 5);
+	}
+	
+	public static void main(String[] args)
+	{
+		Functions test = new Functions();
+		test.run();
 	}
 }
