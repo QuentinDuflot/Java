eeedcf5f810b48d6bd1b7eb9da0ac01bbbbb38df
Marie-Lou Wechsler <marie-lou.wechsler@live.fr>
2019-07-14 17:53:18 +0200

    Quick test dialog

--- ueb24/car/tester.c
+++ ueb24/car/tester.c
@@ -4,6 +4,7 @@
 #define SHOW_GARAGE 1
 #define ADD_CAR 2
 #define REMOVE_CAR 3
+#define QUICK_TESTER 4
 #define QUIT 0
 
 double readValueDouble(char *message) {
@@ -41,9 +42,31 @@ void printMenu() {
         "\t%d. Show garage\n"
         "\t%d. Add new car to garage\n"
         "\t%d. Remove car from garage\n"
+        "\t%d. Quick test\n"
         "\t%d. Quit program\n",
-        SHOW_GARAGE, ADD_CAR, REMOVE_CAR, QUIT
+        SHOW_GARAGE, ADD_CAR, REMOVE_CAR, QUICK_TESTER, QUIT
     );
+}
+
+void quickTest() {
+    CAR carsArray[5];
+	int numberOfCars = 0;
+    char extras[10][100] = {"Climatisation", "Cabriolet", "Cruise Control"};
+
+    CAR c1 = createCar("BMW", 120, 5, true, extras[0], 150, 6, 2340);
+    CAR c2 = createCar("Porsche", 350, 3, false, extras[0], 150, 6, 2340);
+    CAR c3 = createCar("Skoda", 140, 5, true, extras[0], 150, 6, 2340);
+    CAR c4 = createCar("Volkswagen", 260, 5, true, extras[0], 150, 6, 2340);
+    CAR c5 = createCar("Renault", 110, 3, false, extras[0], 150, 6, 2340);
+
+    addCar(c1, carsArray, 0, &numberOfCars);
+    addCar(c2, carsArray, 1, &numberOfCars);
+    addCar(c3, carsArray, 2, &numberOfCars);
+    printAllCars(carsArray, numberOfCars);
+
+    removeCar(carsArray, 1, &numberOfCars);
+    printf("\n• • • REMOVING CARS • • •\n");
+    printAllCars(carsArray, numberOfCars);
 }
 
 int main() {
@@ -88,8 +111,12 @@ int main() {
             break;
 
         case REMOVE_CAR:
-            int pos = readValueChar("Which position would you like to remove?");
-            removeCar(carsArray, pos, numberOfCars);
+            /*char pos = readValueChar("Which position would you like to remove?");
+            removeCar(carsArray, (int)pos, numberOfCars);*/
+            break;
+
+        case QUICK_TESTER:
+            quickTest();
             break;
         
         case QUIT:
@@ -104,26 +131,3 @@ int main() {
         return 0;
     } while (choice != 0);
 }
-
-/*int main(void) {
-
-    char extras[10][100] = {"Climatisation", "Cabriolet", "Cruise Control"};
-    CAR carsArray[5];
-	int numberOfCars = 0;
-
-    CAR c1 = createCar("BMW", 120, 5, true, extras[0], 150, 6, 2340);
-    CAR c2 = createCar("Porsche", 350, 3, false, extras[0], 150, 6, 2340);
-    CAR c3 = createCar("Skoda", 140, 5, true, extras[0], 150, 6, 2340);
-    CAR c4 = createCar("Volkswagen", 260, 5, true, extras[0], 150, 6, 2340);
-    CAR c5 = createCar("Renault", 110, 3, false, extras[0], 150, 6, 2340);
-
-    addCar(c1, carsArray, 0, &numberOfCars);
-    addCar(c2, carsArray, 1, &numberOfCars);
-    addCar(c3, carsArray, 2, &numberOfCars);
-
-    printAllCars(carsArray, numberOfCars);
-
-    removeCar(carsArray, 1, &numberOfCars);
-    printf("\nREMOVING CARS\n");
-    printAllCars(carsArray, numberOfCars);
-}*/
